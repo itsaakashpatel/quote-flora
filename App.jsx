1652267f809bf1ba872e2bf83e08e981f6893253
Aakash Patel <aakashpatel25274@gmail.com>
2023-10-22 20:26:16 -0400

    [#1 ] Data of quotes, authors, and categories
    Add required data to show quotes and their categories and authors in a JSON format.

--- App.jsx
+++ App.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import { Button, Text } from "react-native-paper";
+import { Text } from "react-native-paper";
+import { quotes } from "./src/data";
 
 function App() {
+  const firstQuote = quotes[0] || "No quotes found";
+  console.log(firstQuote);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app z!</Text>
+      <Text variant="displayMedium">Quote of the day</Text>
+      {firstQuote && <Text>{JSON.stringify(firstQuote)}</Text>}
       <StatusBar style="auto" />
-      <Button primary onPress={() => console.log("Pressed")} mode="contained">
-        Demo Button
-      </Button>
     </View>
   );
 }
