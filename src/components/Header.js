44b4cbd18d9dd82542d306b47f429e223c69682a
Jenil17 <jeniljariwala64@gmail.com>
2023-10-24 15:22:53 -0400

    [#2] - Get A Random Quote

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <View>
+      <Text style={styles.txt}>Quotes</Text>
+    </View>
+  )
+}
+
+export default Header
+
+const styles = StyleSheet.create({
+    txt:{
+        fontWeight: 'bold',
+        fontSize: 30,
+        color:'#BDBDBD'
+    }
+})
---
+++ src/screens/Categories.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+//List of categories
+//Click will open a new screen which shows all quotes of that category
+
+const Categories = () => {
+  return (
+    <View>
+      <Text>Categories</Text>
+    </View>
+  )
+}
+
+export default Categories
+
+const styles = StyleSheet.create({})
