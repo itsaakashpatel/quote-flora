4b7aa80b0a629079756390d6886df1d95b5129cc
Ashika213 <aa.ishika213@gmail.com>
2023-10-24 20:11:47 -0400

    category changes

--- babel.config.js
+++ babel.config.js
@@ -7,6 +7,5 @@ module.exports = function (api) {
         plugins: ["react-native-paper/babel"],
       },
     },
-    plugins: ["react-native-reanimated/plugin"],
   };
 };
--- src/screens/Categories.js
+++ src/screens/Categories.js
@@ -1,17 +1,63 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 
-//List of categories
-//Click will open a new screen which shows all quotes of that category
+const Categories = ({ navigation }) => {
+  const [categoryData, setCategoryData] = useState([]);
 
-const Categories = () => {
+  useEffect(() => {
+    // Function to fetch data from categories.json
+    const fetchData = async () => {
+      try {
+        const response = await fetch('categories.json'); // Adjust the path to your JSON file
+        const data = await response.json();
+        setCategoryData(data);
+      } catch (error) {
+        console.error('Error fetching category data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const navigateToCategory = (category) => {
+    // Your navigation logic here
+    // For example, use React Navigation to navigate to a category-specific screen
+    // navigation.navigate('CategoryScreen', { category });
+  };
+
   return (
-    <View>
-      <Text>Categories</Text>
+    <View style={styles.container}>
+      <Text style={styles.header}>Categories</Text>
+      <FlatList
+        data={categoryData}
+        keyExtractor={(item) => item._id}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => navigateToCategory(item.name)}>
+            <View style={styles.categoryItem}>
+              <Text>{item.name}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Categories
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  categoryItem: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+});
 
-const styles = StyleSheet.create({})
+export default Categories;
