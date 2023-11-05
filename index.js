2e25802e1f281791bfe1a0ad9ce55871db9ae0ac
Aakash Patel <hi@aakashpatel.in>
2023-11-05 13:11:34 -0500

    Code formatted
    Closes [#3]

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {registerRootComponent} from 'expo';
 import {Provider as PaperProvider} from 'react-native-paper';
 
-import App from "./App";
+import App from './App';
 
 const Main = () => {
   return (
