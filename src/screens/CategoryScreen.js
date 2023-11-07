4c2e9a377aa34e2ef053c830bc2a1bacbf88522e
Ashika213 <aa.ishika213@gmail.com>
2023-11-06 21:53:06 -0500

    Favourite a Quote

--- src/screens/CategoryScreen.js
+++ src/screens/CategoryScreen.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, StyleSheet, FlatList} from 'react-native';
 import QuoteCard from '../components/QuoteCard';
-import {quotes} from '../data';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const CategoryScreen = ({route}) => {
   const {categoryName} = route.params;
+  const [allQuotes, setAllQuotes] = useState([]);
 
-  const quotesForCategory = quotes.filter((quote) => quote.categories.includes(categoryName));
+  useEffect(() => {
+    AsyncStorage.getItem('quotes')
+      .then((jsonQuotes) => {
+        if (jsonQuotes) {
+          const parsedQuotes = JSON.parse(jsonQuotes);
+          setAllQuotes(parsedQuotes);
+        }
+      })
+      .catch((error) => console.error('Error loading quotes:', error));
+  }, []);
+
+  const quotesForCategory = allQuotes.filter((quote) => quote.categories.includes(categoryName));
+
+  function deleteQuote(quoteId) {
+    const updatedQuotes = allQuotes.filter((quote) => quote._id !== quoteId);
+    setAllQuotes(updatedQuotes);
+    AsyncStorage.setItem('quotes', JSON.stringify(updatedQuotes))
+      .then(() => {
+        console.log('Quote deleted and quotes updated successfully');
+      })
+      .catch((error) => console.error('Error deleting quote:', error));
+  }
+
+  function favouriteQuoteHandler(value) {
+    const updatedLikedQuotes = allQuotes.reduce((accumulator, currentQuote) => {
+      if (currentQuote._id === value.id) {
+        currentQuote.isLiked = value.isLiked; // Update isLiked property
+      }
+      return [...accumulator, currentQuote];
+    }, []);
+
+    setAllQuotes(updatedLikedQuotes);
+    AsyncStorage.setItem('quotes', JSON.stringify(updatedLikedQuotes))
+      .then(() => {
+        console.log('Quote updated and quotes updated successfully');
+      })
+      .catch((error) => console.error('Error in updating quote:', error));
+  }
 
   return (
     <View style={styles.container}>
@@ -14,7 +52,13 @@ const CategoryScreen = ({route}) => {
       <FlatList
         data={quotesForCategory}
         keyExtractor={(item) => item._id}
-        renderItem={({item}) => <QuoteCard quote={item} />}
+        renderItem={({item}) => (
+          <QuoteCard
+            quote={item}
+            onDelete={deleteQuote}
+            favouriteQuoteHandler={favouriteQuoteHandler}
+          />
+        )}
       />
     </View>
   );
--- src/screens/Favourite.js
+++ src/screens/Favourite.js
@@ -1,14 +1,99 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  SafeAreaView,
+  FlatList,
+  RefreshControl,
+} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import QuoteCard from '../components/QuoteCard';
+import Header from '../components/Header';
 
 const Favourite = () => {
+  const [likedQuotes, setLikedQuotes] = useState([]);
+  const [allQuotes, setAllQuotes] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
+
+  function fetchQuotes() {
+    AsyncStorage.getItem('quotes')
+      .then((jsonQuotes) => {
+        if (jsonQuotes) {
+          const parsedQuotes = JSON.parse(jsonQuotes);
+          setAllQuotes(parsedQuotes);
+          const filterLikedQuotes = parsedQuotes.filter((quote) => quote.isLiked);
+          setLikedQuotes([...filterLikedQuotes]);
+          console.log('Fetched quotes!');
+        }
+      })
+      .catch((error) => console.error('Error loading quotes:', error));
+  }
+
+  function deleteQuote(quoteId) {
+    const updatedQuotes = allQuotes.filter((quote) => quote._id !== quoteId);
+    setAllQuotes(updatedQuotes);
+    AsyncStorage.setItem('quotes', JSON.stringify(updatedQuotes))
+      .then(() => {
+        console.log('Quote deleted and quotes updated successfully');
+      })
+      .catch((error) => console.error('Error deleting quote:', error));
+  }
+
+  function favouriteQuoteHandler(value) {
+    const updatedLikedQuotes = allQuotes.reduce((accumulator, currentQuote) => {
+      if (currentQuote._id === value.id) {
+        currentQuote.isLiked = value.isLiked; // Update isLiked property
+      }
+      return [...accumulator, currentQuote];
+    }, []);
+
+    setAllQuotes(updatedLikedQuotes);
+    AsyncStorage.setItem('quotes', JSON.stringify(updatedLikedQuotes))
+      .then(() => {
+        console.log('Quote updated and quotes updated successfully');
+      })
+      .catch((error) => console.error('Error in updating quote:', error));
+
+    fetchQuotes();
+  }
+
+  const handleRefresh = () => {
+    setRefreshing(true);
+    fetchQuotes(); // Fetch the liked quotes
+    setRefreshing(false);
+  };
+
   return (
-    <View>
-      <Text>Favourite</Text>
-    </View>
+    <SafeAreaView>
+      <Header text={'Favourite Quotes'} />
+      <FlatList
+        style={styles.container}
+        data={likedQuotes}
+        keyExtractor={(item, index) => item._id.toString()}
+        renderItem={({item, index}) => (
+          <QuoteCard
+            key={index}
+            quote={item}
+            onDelete={deleteQuote}
+            favouriteQuoteHandler={favouriteQuoteHandler}
+          />
+        )}
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />}
+      />
+    </SafeAreaView>
   );
 };
 
-export default Favourite;
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F0F4FE',
+  },
+});
 
-const styles = StyleSheet.create({});
+export default Favourite;
