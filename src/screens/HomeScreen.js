5402980e291dea545789afed51c2a17ca9fbea0a
Aakash Patel <hi@aakashpatel.in>
2023-11-27 21:22:47 -0500

    Duplicate function removed and allquotes dependency removed

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -41,7 +41,7 @@ const HomeScreen = () => {
       }
       setRandomQuoteIndices(newIndices);
     }
-  }, [allQuotes]);
+  }, []);
 
   function getRandomQuoteIndex() {
     return Math.floor(Math.random() * allQuotes.length);
@@ -89,32 +89,15 @@ const HomeScreen = () => {
       console.error('Quote not found for updating rating');
     }
   }
-  //!
-  function updateRating(updatedQuote) {
-    const quoteIndex = allQuotes.findIndex((quote) => quote._id === updatedQuote._id);
-
-    // If the quote is found, update the rating
-    if (quoteIndex !== -1) {
-      // Update the quote in the array
-      allQuotes[quoteIndex] = updatedQuote;
-
-      // Save the updated quotes to AsyncStorage
-      AsyncStorage.setItem('quotes', JSON.stringify(allQuotes))
-        .then(() => {
-          console.log('Rating updated and quotes saved successfully');
-        })
-        .catch((error) => console.error('Error updating rating:', error));
-    } else {
-      console.error('Quote not found for updating rating');
-    }
-  }
 
   function favouriteQuoteHandler(value) {
-    const updatedLikedQuotes = allQuotes.map((currentQuote) =>
-      currentQuote._id === value.id ? {...currentQuote, isLiked: value.isLiked} : currentQuote,
-    );
-
-    setAllQuotes(updatedLikedQuotes);
+    const updatedLikedQuotes = allQuotes.reduce((accumulator, currentQuote) => {
+      if (currentQuote._id === value.id) {
+        currentQuote.isLiked = value.isLiked; // Update isLiked property
+      }
+      return [...accumulator, currentQuote];
+    }, []);
+    setAllQuotes([...updatedLikedQuotes]);
 
     AsyncStorage.setItem('quotes', JSON.stringify(updatedLikedQuotes))
       .then(() => {
@@ -145,6 +128,7 @@ const HomeScreen = () => {
                 quote={allQuotes[index]}
                 onDelete={deleteQuote}
                 favouriteQuoteHandler={favouriteQuoteHandler}
+                updateRating={updateRating}
               />
             ))}
           </ScrollView>
@@ -155,18 +139,6 @@ const HomeScreen = () => {
           <Text>No quotes available</Text>
         </View>
       )}
-      <ScrollView>
-        {randomQuoteIndices.map((index) => (
-          <QuoteCard
-            key={index}
-            quote={allQuotes[index]}
-            onDelete={deleteQuote}
-            favouriteQuoteHandler={favouriteQuoteHandler}
-            updateRating={updateRating}
-          />
-        ))}
-      </ScrollView>
-      <MainButton title="Get New Quotes" onPress={changeRandomQuotes} />
     </SafeAreaView>
   );
 };
